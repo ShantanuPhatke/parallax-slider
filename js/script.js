f5f20555fde3434934364ccd00299772c66b7c47
shantanuphatke <sphatkes@gmail.com>
2022-05-28 19:01:44 +0530

    fix: animation not playing

--- js/script.js
+++ js/script.js
@@ -1,115 +1,122 @@
-const select = element => document.querySelector(element);
+const select = (element) => document.querySelector(element)
 
-const getTranslateX = element => {
-    let style = window.getComputedStyle(element);
-    let matrix = new WebKitCSSMatrix(style.transform);
-    return matrix.m41;
+const getTranslateX = (element) => {
+  let style = window.getComputedStyle(element)
+  let matrix = new WebKitCSSMatrix(style.transform)
+  return matrix.m41
 }
 
-const nextSlide = imgNo => {
-    let currentTranslateX = getTranslateX(select(`#img-${imgNo}`));
-    select(`#img-${imgNo}`).style.transform = `translateX(calc(${currentTranslateX}px - 16.67%))`;
+const nextSlide = (imgNo) => {
+  let currentTranslateX = getTranslateX(select(`#img-${imgNo}`))
+  select(
+    `#img-${imgNo}`
+  ).style.transform = `translateX(calc(${currentTranslateX}px - 16.67%))`
 }
 
-const previousSlide = imgNo => {
-    let currentTranslateX = getTranslateX(select(`#img-${imgNo}`));
-    select(`#img-${imgNo}`).style.transform = `translateX(calc(${currentTranslateX}px + 16.67%))`;
+const previousSlide = (imgNo) => {
+  let currentTranslateX = getTranslateX(select(`#img-${imgNo}`))
+  select(
+    `#img-${imgNo}`
+  ).style.transform = `translateX(calc(${currentTranslateX}px + 16.67%))`
 }
 
-const disableClick = () => select("#parallax_slider").style.pointerEvents = "none";
+const disableClick = () =>
+  (select("#parallax_slider").style.pointerEvents = "none")
 
-const enableClick = () => select("#parallax_slider").style.pointerEvents = "auto";
+const enableClick = () =>
+  (select("#parallax_slider").style.pointerEvents = "auto")
 
-const removeScrollListener = () => document.removeEventListener('scroll', initAnimation);
+const removeScrollListener = () =>
+  document.removeEventListener("scroll", initAnimation)
 
-let counter = 0;
+let counter = 0
 
 const initAnimation = () => {
-    if (counter == 0 && isInViewport()) {
-        const refreshInterval = setInterval(() => {
-            if (isInViewport()) {
-                counter++;
-                if (counter === 5) {
-                    select("#previous-btn").style.visibility = "visible";
-                    select("#next-btn").style.visibility = "hidden";
-                    clearInterval(refreshInterval);
-                }
-                nextSlide(3);
-                setTimeout(() => {
-                    nextSlide(2);
-                }, 200);
-                setTimeout(() => {
-                    nextSlide(1);
-                }, 400);
-            }
-        }, 2500);
+  console.log("initAnimation")
+  if (counter == 0 && isInViewport()) {
+    console.log("inViewPort")
+    const refreshInterval = setInterval(() => {
+      if (isInViewport()) {
+        counter++
+        if (counter === 5) {
+          select("#previous-btn").style.visibility = "visible"
+          select("#next-btn").style.visibility = "hidden"
+          clearInterval(refreshInterval)
+        }
+        nextSlide(3)
+        setTimeout(() => {
+          nextSlide(2)
+        }, 200)
+        setTimeout(() => {
+          nextSlide(1)
+        }, 400)
+      }
+    }, 2500)
 
-        removeScrollListener();
-    }
+    removeScrollListener()
+  }
 }
 
 const isInViewport = () => {
-    const parallax_slider = select("#parallax_slider");
-    const rect = parallax_slider.getBoundingClientRect();
-    return (
-        rect.top >= 0 &&
-        rect.left >= 0 &&
-        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-    );
+  const parallax_slider = select("#parallax_slider")
+  const rect = parallax_slider.getBoundingClientRect()
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  )
 }
 
-document.addEventListener('scroll', initAnimation);
+document.addEventListener("DOMContentLoaded", initAnimation)
 
 select("#next-btn").addEventListener("click", () => {
-    disableClick(); // Disables clicks on the slider as the animation starts
+  disableClick() // Disables clicks on the slider as the animation starts
 
-    counter++;
-    if (counter != 0) {
-        select("#previous-btn").style.visibility = "visible";
-    }
-    if (counter === 5) {
-        select("#next-btn").style.visibility = "hidden";
-    }
+  counter++
+  if (counter != 0) {
+    select("#previous-btn").style.visibility = "visible"
+  }
+  if (counter === 5) {
+    select("#next-btn").style.visibility = "hidden"
+  }
 
-    nextSlide(3);
-    setTimeout(() => {
-        nextSlide(2);
-    }, 300);
-    setTimeout(() => {
-        nextSlide(1);
-    }, 400);
-    
-    // Enables clicks on the slider after animation is completed 
-    setTimeout(() => {
-        enableClick();
-    }, 1400);
+  nextSlide(3)
+  setTimeout(() => {
+    nextSlide(2)
+  }, 300)
+  setTimeout(() => {
+    nextSlide(1)
+  }, 400)
 
-});
+  // Enables clicks on the slider after animation is completed
+  setTimeout(() => {
+    enableClick()
+  }, 1400)
+})
 
 select("#previous-btn").addEventListener("click", () => {
-    disableClick(); // Disables clicks on the slider as the animation starts
+  disableClick() // Disables clicks on the slider as the animation starts
 
-    counter--;
-    if (counter != 5) {
-        select("#next-btn").style.visibility = "visible";
-    }
-    if (counter === 0) {
-        select("#previous-btn").style.visibility = "hidden";
-    }
+  counter--
+  if (counter != 5) {
+    select("#next-btn").style.visibility = "visible"
+  }
+  if (counter === 0) {
+    select("#previous-btn").style.visibility = "hidden"
+  }
 
-    previousSlide(3);
-    setTimeout(() => {
-        previousSlide(2);
-    }, 300);
-    setTimeout(() => {
-        previousSlide(1);
-    }, 400);
+  previousSlide(3)
+  setTimeout(() => {
+    previousSlide(2)
+  }, 300)
+  setTimeout(() => {
+    previousSlide(1)
+  }, 400)
 
-    // Enables clicks on the slider after animation is completed 
-    setTimeout(() => {
-        enableClick();
-    }, 1400);
-
-});
-
+  // Enables clicks on the slider after animation is completed
+  setTimeout(() => {
+    enableClick()
+  }, 1400)
+})
